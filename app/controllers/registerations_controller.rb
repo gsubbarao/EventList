5122022c48919394443d319adb2a3aa5fbc20dec
subbarao <subbarao.kly@gmail.com>
2013-07-04 16:33:47 +0530

    User basic registeration and login Completed

--- app/controllers/registerations_controller.rb
+++ app/controllers/registerations_controller.rb
@@ -1,13 +1,22 @@
 class RegisterationsController < ApplicationController
-	
-	def new
-		@user = User.new
-	end
+  
+  def new
+    @user = User.new
+  end
 
-	def create
-	end
+  def create
+    @user = User.new(params[:user])
+      if @user.save
+        redirect_to root_url, :notice => "Signed up!"
+      else
+        render "new"
+      end
+  end
 
-	def destroy
-	end
+  def destroy
+    User.destroy(session[:user_id])
+    session[:user_id] = nil
+    redirect_to root_url, :notice => "You have removed your account, please register again to participate in Event."
+  end
 
 end
